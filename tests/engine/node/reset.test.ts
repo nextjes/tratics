cdc441063cca1819f0801015de550475cdf7d63f
joowan217 <rlawndhks217@gmail.com>
2025-03-18 10:34:16 +0900

    c: jest 제거 & vitest 추가

---
+++ tests/engine/node/reset.test.ts
@@ -0,0 +1,12 @@
+import { expect, test, describe } from "vitest";
+import { Node } from "../../../app/engine/Node";
+
+describe("", () => {
+  test("is reset", () => {
+    let node = new Node(3);
+
+    node.reset();
+
+    expect(node).toEqual(new Node(3));
+  });
+});
