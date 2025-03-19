176ce24b3460f40152d3dc7b191f3231d19dbdc8
joowan217 <rlawndhks217@gmail.com>
2025-03-19 10:28:10 +0900

    u: alias 복원

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import svgr from "vite-plugin-svgr";
-import path from "path";
 
 export default defineConfig({
   plugins: [svgr(), tailwindcss(), reactRouter(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "./app"),
-      "@app": path.resolve(__dirname, "./app"),
-    },
-  },
 });
