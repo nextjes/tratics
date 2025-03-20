512c96a4988de14f3b20a5c66f566ea9a2855a29
joowan217 <rlawndhks217@gmail.com>
2025-03-20 17:57:17 +0900

    c: 상수 추가

--- app/engine/index.ts
+++ app/engine/index.ts
@@ -1,3 +1,4 @@
 import * as time from "./term/time";
+import * as constants from "./term/constants";
 
-export const term = { ...time };
+export const term = { ...time, ...constants };
---
+++ app/engine/term/constants.ts
@@ -0,0 +1,13 @@
+export const enum Role {
+  Clock = "clock",
+  Node = "node",
+  Task = "task",
+}
+
+export const enum TaskStatus {
+  New = "new",
+  Runnable = "runnable",
+  Running = "running",
+  Waiting = "waiting",
+  Terminated = "terminated",
+}
