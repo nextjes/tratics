061fd855002d1eeff0ebad2fd5c69107e9b23cdd
joowan217 <rlawndhks217@gmail.com>
2025-03-20 10:06:56 +0900

    u: private 접근자 표기 수정

--- app/engine/updatable.ts
+++ app/engine/updatable.ts
@@ -9,23 +9,23 @@ export interface Updatable {
 
 export class Node implements Updatable {
   estimatedProcessingDuration: number;
-  private elapsedTime: number;
-  private status: string;
+  #elapsedTime: number;
+  #status: string;
 
   constructor(estimatedProcessingDuration: number) {
     this.estimatedProcessingDuration = estimatedProcessingDuration;
-    this.elapsedTime = 0.0;
-    this.status = "idle";
+    this.#elapsedTime = 0.0;
+    this.#status = "idle";
   }
 
   update(deltaTime: number): void {
-    this.elapsedTime += deltaTime;
-    console.log(`Node updated: value = ${this.elapsedTime.toFixed(4)}`);
-    useMemoryState.getState().setNodeStatus(this.status);
+    this.#elapsedTime += deltaTime;
+    console.log(`Node updated: value = ${this.#elapsedTime.toFixed(4)}`);
+    useMemoryState.getState().setNodeStatus(this.#status);
   }
 
   reset(): void {
-    this.elapsedTime = 0.0;
-    this.status = "idle";
+    this.#elapsedTime = 0.0;
+    this.#status = "idle";
   }
 }
