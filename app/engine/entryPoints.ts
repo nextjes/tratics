23df2f60397c250c52e23c7a4f320e1f8316b439
joowan217 <rlawndhks217@gmail.com>
2025-03-20 16:50:23 +0900

    u: 주석 제거

--- app/engine/entryPoints.ts
+++ app/engine/entryPoints.ts
@@ -3,7 +3,6 @@ import { Node, SimulationClock, type Temporal } from "./updatable";
 import { useMemoryState } from "~/store/memory";
 import * as term from "./term";
 
-// SimulationEngine 클래스: 업데이트 루프를 관리하며 start와 pause 함수 제공
 class SimulationEngine {
   private intervalId: ReturnType<typeof setInterval> | null = null;
   private tickInterval: number;
@@ -14,14 +13,10 @@ class SimulationEngine {
     this.temporals = temporals;
   }
 
-  // start() 함수: 시뮬레이션 업데이트 루프 시작
   start(): void {
-    if (this.intervalId !== null) return; // 이미 실행 중이면 중복 실행하지 않음
+    if (this.intervalId !== null) return;
     this.intervalId = setInterval(() => {
-      // 클럭 업데이트 및 deltaTime 계산
       const deltaTime = new term.MilliSecond(this.tickInterval);
-
-      // 등록된 모든 도메인 객체 업데이트
       this.temporals = this.temporals.map((obj) => obj.after(deltaTime));
       new Scene(this.temporals).publish();
     }, this.tickInterval);
