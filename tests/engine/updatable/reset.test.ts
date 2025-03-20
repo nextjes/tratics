84d4a2ceafc9594bd2492283bfb1740e9858b4f2
joowan217 <rlawndhks217@gmail.com>
2025-03-20 10:31:45 +0900

    u: 모듈명 변경

--- tests/engine/updatable/reset.test.ts
+++ tests/engine/updatable/reset.test.ts
@@ -1,5 +1,5 @@
 import { expect, test, describe } from "vitest";
-import { Node } from "~/engine/node";
+import { Node } from "~/engine/updatable";
 
 describe("", () => {
   test("is reset", () => {
