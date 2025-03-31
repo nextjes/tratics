5d0487eee47f1b0ca6065c82951fcdc7852dd1e5
alice <jyynkr92@gmail.com>
2025-03-31 10:50:01 +0900

    Tratics 31 (#33)
    * c: add isStarted state
    
    * c: add server spec design
    
    * u: simultaion and status shows only on started and add serverspec component

--- app/client/main/Main.tsx
+++ app/client/main/Main.tsx
@@ -1,21 +1,26 @@
 import { stop, start } from "~/engine/entryPoints";
 import Header from "./Header";
 import Section from "./Section";
+import { useState } from "react";
 
 const Main = () => {
+  const [isStarted, setIsStarted] = useState(false);
+
   const onStartClick = (requestCounts: number): void => {
     console.log(requestCounts);
     start();
+    setIsStarted(true);
   };
 
   const onStopClick = (): void => {
     stop();
+    setIsStarted(false);
   };
 
   return (
     <main>
       <Header onStartClick={onStartClick} onStopClick={onStopClick} />
-      <Section />
+      <Section isStarted={isStarted} />
     </main>
   );
 };
--- app/client/main/Section.tsx
+++ app/client/main/Section.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import ClientIcon from "~/client/images/computer-monitor-svgrepo-com.svg?react";
 import ServerIcon from "~/client/images/server-svgrepo-com.svg?react";
 import { useNodeStatus, useMemoryClock, useLinkStatus } from "~/store/memory";
+import ServerSpec from "../ui/ServerSpec";
 
-const Section = () => {
+interface SectionProps {
+  isStarted: boolean;
+}
+
+const Section = ({ isStarted }: SectionProps) => {
   const clock = useMemoryClock();
   const nodeStatus = useNodeStatus();
   const linkStatus = useLinkStatus();
@@ -30,13 +35,18 @@ const Section = () => {
 
   return (
     <section className="flex flex-col gap-7 justify-center items-center">
+      <ServerSpec isStarted={isStarted} />
       <div className="flex items-center">
         <ClientIcon className="size-[50px]" />
         <ServerIcon className="size-[50px]" />
       </div>
-      <div>Simulation Time : {clock} seconds </div>
-      <div>Node status : {formattedNodeStatus}</div>
-      <div>link status : {formattedLinkStatus}</div>
+      {isStarted && (
+        <>
+          <div>Simulation Time : {clock} seconds </div>
+          <div>Node status : {formattedNodeStatus}</div>
+          <div>link status : {formattedLinkStatus}</div>
+        </>
+      )}
     </section>
   );
 };
