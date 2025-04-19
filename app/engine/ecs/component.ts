1361b220bf0fb2a25aeff59020125ae7e4881dfe
joowan217 <rlawndhks217@gmail.com>
2025-04-19 22:12:21 +0900

    c:  시스템 로직 추가

--- app/engine/ecs/component.ts
+++ app/engine/ecs/component.ts
@@ -2,12 +2,16 @@ import { Component, Types } from "ecsy";
 import type { Core } from "./infra";
 
 export class PreEndTimeInDelta extends Component<PreEndTimeInDelta> {
+  value!: number;
+
   static schema = {
     value: { type: Types.Number, default: 0 },
   };
 }
 
 export class Identity extends Component<Identity> {
+  id!: string;
+
   static schema = {
     id: { type: Types.String },
   };
@@ -21,25 +25,53 @@ export class Cores extends Component<Cores> {
   };
 }
 
+export class Message extends Component<Message> {
+  rrcId!: string;
+  srcId!: string;
+  dstId!: string;
+  size!: number;
+  transmittedSize!: number;
+
+  static schema = {
+    rrcId: { type: Types.String },
+    srcId: { type: Types.String },
+    dstId: { type: Types.String },
+    size: { type: Types.Number },
+    transmittedSize: { type: Types.Number },
+  };
+}
+
 export class RequestQueue extends Component<RequestQueue> {
+  requests!: Message[];
+
   static schema = {
     requests: { type: Types.Array },
   };
 }
 
 export class ResponseQueue extends Component<ResponseQueue> {
+  responses!: Message[];
+
   static schema = {
     responses: { type: Types.Array },
   };
 }
 
 export class TaskQueue extends Component<TaskQueue> {
+  tasks!: Message[];
+
   static schema = {
     tasks: { type: Types.Array },
   };
 }
 
 export class LinkSpec extends Component<LinkSpec> {
+  srcId!: string;
+  dstId!: string;
+  bandwidth!: number;
+  latency!: number;
+  reliability!: number;
+
   static schema = {
     srcId: { type: Types.String },
     dstId: { type: Types.String },
@@ -50,7 +82,21 @@ export class LinkSpec extends Component<LinkSpec> {
 }
 
 export class Throughput extends Component<Throughput> {
+  value!: number;
+
   static schema = {
     value: { type: Types.Number },
+  };
+}
+
+export class Task extends Component<Task> {
+  rrcId!: string;
+  duration!: number;
+  elapsed!: number;
+
+  static schema = {
+    rrcId: { type: Types.String },
+    duration: { type: Types.Number },
+    elapsed: { type: Types.Number },
   };
 }
--- app/engine/ecs/system.ts
+++ app/engine/ecs/system.ts
@@ -1,4 +1,8 @@
-import { System } from "ecsy";
+import { Entity, System } from "ecsy";
+import { useMemoryState } from "~/store/memory";
+import { Server, Link } from "./tag";
+import { Cores, Identity, LinkSpec, Throughput } from "./component";
+import type { Core } from "./infra";
 
 export class TrafficGeneration extends System {
   execute(delta: number, time: number): void {
@@ -37,7 +41,32 @@ export class CleanPreEndTimeInDelta extends System {
 }
 
 export class PerformanceIndicatorRelease extends System {
+  static queries = {
+    nodes: { components: [Server] },
+    links: { components: [Link] },
+  };
+
   execute(delta: number, time: number): void {
-    // Implement the logic for releasing indicators
+    const { setClock, setNodeStatus, setLinkStatus } =
+      useMemoryState.getState();
+
+    setClock((time / 1000).toFixed(1));
+    const newNodeStates = this.queries.nodes.results.map((entity: Entity) => ({
+      id: entity.getComponent(Identity)!.id,
+      cores: entity.getComponent(Cores)!.value.length,
+      busyCores: entity
+        .getComponent(Cores)!
+        .value.filter((core: Core) => core.taskId !== null).length,
+    }));
+    setNodeStatus(newNodeStates);
+
+    const activeLinks = this.queries.links.results.map((entity: Entity) => ({
+      id: entity.getComponent(Identity)!.id,
+      srcId: entity.getComponent(LinkSpec)!.srcId,
+      dstId: entity.getComponent(LinkSpec)!.dstId,
+      bandwidth: entity.getComponent(LinkSpec)!.bandwidth,
+      throughput: entity.getComponent(Throughput)!.value,
+    }));
+    setLinkStatus(activeLinks);
   }
 }
